0f08378f2e08690c1a6674248b8312a7569e1a9c
paran1980 <43312377+paran1980@users.noreply.github.com>
2023-05-28 20:43:32 +0530

    Feature/feature1 (#7)
    * Checkin from feature/feature1 branch 560076
    
    * Commit from Feature/feature  branch of paran1980 account with changes at line14

--- src/main/scala/DevObject.scala
+++ src/main/scala/DevObject.scala
@@ -9,10 +9,12 @@ object DevObject {
     print("Dev object line 4  - Bharathi parandhaman-102030")
     //checkin from the feature/feature1 branch
     print("Dev object line 5 - paran1980-560076")
+    //checkin from feature/feature1 branch of paran1980 account
+    //change 12456
+    print("Dev object line 6  - paran1980-789")
     //checkin from bharathi parandhaman forked master branch
     //change 12
     print("Dev object line 6  - Bharathi parandhaman-456")
-    
 
   }
 }
