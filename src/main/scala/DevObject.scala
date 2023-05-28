8ea264f0092dc6d0e40ef9d6b64e8bcad3ee82f8
BharathiParan <130691270+BharathiParan@users.noreply.github.com>
2023-05-28 19:10:01 +0530

    Update DevObject.scala
    Added  changes at line 6

--- src/main/scala/DevObject.scala
+++ src/main/scala/DevObject.scala
@@ -3,6 +3,7 @@ package org.paran.appln
 object DevObject {
   def main(args: Array[String]): Unit = {
     print("Dev object - Bharathi parandhaman-123")
+    print("Dev object line 2 - Bharathi Parandhama-12345")
     //checkin from bharathi parandhaman
 
   }
